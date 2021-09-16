dcf33836ba2593ef09e12982fcdd9376de0ffc6c
Helen Sung <helenjeehosung@gmail.com>
2021-09-16 13:50:51 -0400

    fix: yellow component in route

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const Main = (props) => {
         <Route path="/red" component={Red} />
         <Route path="/green" component={Green} />
 
-        <Route path="/gray" component={Gray} />
+        <Route path="/yellow" component={Yellow} />
 
         <Route exact path="/" component={Red} />
       </div>
