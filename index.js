b4cf9db47c61d91809ac0e04f74dfde6c9fa3f33
Helen Sung <helenjeehosung@gmail.com>
2021-09-16 13:47:23 -0400

    change last color to yellow

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ const Green = (props) => {
   );
 };
 
-const Gray = (props) => {
+const Yellow = (props) => {
   return (
-    <div className="gray">
-      <h1>Gray</h1>
+    <div className="yellow">
+      <h1>Yellow</h1>
     </div>
   );
 };
@@ -43,7 +43,7 @@ const Main = (props) => {
         <Link to="/red">Go to Red</Link>
         <Link to="/green">Go to Green</Link>
 
-        <Link to="/gray">Go to Gray</Link>
+        <Link to="/yellow">Go to Yellow</Link>
       </div>
 
       <div id="main-section">
